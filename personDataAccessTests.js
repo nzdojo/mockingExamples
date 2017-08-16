47fb7dea826d3f7c132823ea5709691bbec04378
Teamzone <teamzone@aboutagile.com>
2017-08-16 01:08:18 +0000

    Added the infrastructural code for sinon, using the database api as a guide to what we want to be checking for.  The test is failing.  We have not implementation.  We will also need to install the database. We will use constructor injection

--- personDataAccessTests.js
+++ personDataAccessTests.js
@@ -1,17 +1,22 @@
 var assert = require('assert'); 
 var PersonDataAccess = require('./personDataAccess');
-var Database = require('./Database.js');  // this will be faked, need a reference to it
+var Database = require('./database.js');  // this will be faked, need a reference to it
 var sinon = require('sinon');
 require('mocha-sinon');
 
 describe("Testing function the save personal details.", function () {
 
     it('Add a new person to the database', function (done) {
+        var sandbox = sinon.sandbox.create();
+        var database = new Database();
+        var stubWritePerson = sandbox.stub(database, 'WritePerson');
+        stubWritePerson.yields();
+        
         var personDataAccess = new PersonDataAccess();
         var person = { Name: 'Tony', LastName: 'Bennett' };
         personDataAccess.Add(person);
         
-        assert(false, 'call to save data failed');
+        assert(stubWritePerson.calledWith(person.Name, person.LastName), 'Write Person not called correctly');
         done();
     });
     
