d35a15654db478cd9c96a65faf448318d82b20d1
Teamzone <teamzone@aboutagile.com>
2017-08-16 00:24:47 +0000

    Added failing test

---
+++ personDataAccessTests.js
@@ -0,0 +1,9 @@
+var assert = require('assert'); 
+
+describe("Testing function the save personal details.", function () {
+
+    it('Add a new person to the database', function (done) {
+        assert(false, "To be implemented");
+    });
+    
+});
