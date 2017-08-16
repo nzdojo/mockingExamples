53cd85ad65a013d55b07a0e7c74bcd01412a6a28
Teamzone <teamzone@aboutagile.com>
2017-08-16 00:29:10 +0000

    We know we need an object to implement so added - we thought about how that may look and decoded on the names chosen.  Still have a more to getting it working

--- personDataAccessTests.js
+++ personDataAccessTests.js
@@ -1,8 +1,11 @@
 var assert = require('assert'); 
+var PersonDataAccess = require('./personDataAccess');
 
 describe("Testing function the save personal details.", function () {
 
     it('Add a new person to the database', function (done) {
+        var personDataAccess = new PersonDataAccess();
+        personDataAccess.Add(person);
         assert(false, "To be implemented");
     });
     
